14f3c418b7c8d3add5e97c2d80b06063859c0e52
Kristián Kunc <kristian.kunc@gmail.com>
2024-03-29 00:37:24 +0100

    make homepage nice grid

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,11 +4,11 @@
 	export let data: PageData;
 </script>
 
-<div class="flex-col text-center justify-center">
+<div class="text-center">
 	<h2>Services</h2>
-	<div>
+	<div class="inline-grid">
 		{#each data.services as service}
-			<div class="flex justify-center items-center space-x-4">
+			<div class="grid grid-cols-3 justify-center items-center space-x-4 space-y-4">
 				<h3>{service.name}</h3>
 				<a class="link" href={service.endpoint} target="_blank">{service.endpoint}</a>
 				<a class="btn bg-picton-blue-500" href="/service/{service.id}">View status</a>
